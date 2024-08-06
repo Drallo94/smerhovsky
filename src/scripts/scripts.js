7a71295411f962dc26bcb659bd2ae7b3619fb0af
Sutenvulf <Nether1@seznam.cz>
2024-08-06 16:28:51 +0200

    Build

--- src/scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-// import "./modules/HelloWorld";
-
-// Some cool modules
-import "./modules/Animate";
-import "./modules/ToggleBodyClass";
-import "./modules/ScrollClass";
-import "./modules/References";
-import "./modules/Tabs";
-import "./modules/Dropdown";
